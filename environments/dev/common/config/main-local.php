8ecc0d0a50c629dce466d6da78e736f66e017bd3
Shamsiddin <sh.toshmirzaev@gmail.com>
2019-11-09 21:22:53 +0500

    docker config change and launching app

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'mysql:host=mysql;dbname=internat.uz',
             'username' => 'root',
-            'password' => '',
+            'password' => 'verysecret',
             'charset' => 'utf8',
         ],
         'mailer' => [
