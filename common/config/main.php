73711aef90c7ca0f46b3943a1be49798cee60eaf
Shamsiddin <sh.toshmirzaev@gmail.com>
2019-11-09 23:24:03 +0500

    CMS installation and some configuration

--- common/config/main.php
+++ common/config/main.php
@@ -10,4 +10,21 @@ return [
             'class' => 'yii\caching\FileCache',
         ],
     ],
+
+    'modules' => [
+        'cms' => [ // don`t change module key
+            'class' => '@abdualiym\cms\Module',
+//            'storageRoot' => $params['staticPath'],
+//            'storageHost' => $params['staticHostInfo'],
+            'thumbs' => [ // 'sm' and 'md' keys are reserved
+                'admin' => ['width' => 128, 'height' => 128],
+                'thumb' => ['width' => 320, 'height' => 320],
+            ],
+            'languages' => [
+                0 => 'Русский', // default language
+                1 => 'English',
+                2 => 'O`zbek tili',
+            ]
+        ],
+    ],
 ];
